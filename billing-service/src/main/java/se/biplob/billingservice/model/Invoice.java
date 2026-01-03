ccdb2f4e20fd4e79cf4542fb86b697f51836205b
roy6767 <roy.biplobslu@gmail.com>
2026-01-03 22:45:42 +0100

    Removed gRpc from billing and configured kafka for asynchronous call

---
+++ billing-service/src/main/java/se/biplob/billingservice/model/Invoice.java
@@ -0,0 +1,42 @@
+package se.biplob.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private UUID bookingId;
+
+    @Column(nullable = false)
+    private UUID patientId;
+
+    @Column(nullable = false)
+    private Long doctorId;
+
+    @Column(nullable = false)
+    private Long treatmentId;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
